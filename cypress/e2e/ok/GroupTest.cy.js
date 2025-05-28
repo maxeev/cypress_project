be7205bd68678fa4f20d7f1a41e398d10ba65755
Alina <114672812+alinaarmut@users.noreply.github.com>
2025-05-29 00:07:01 +0300

    add GroupTest and update e2e.js

---
+++ cypress/e2e/ok/GroupTest.cy.js
@@ -0,0 +1,30 @@
+const username = 'technopol50';
+const password = 'technopolisPassword';
+
+describe('Group Test', () => {
+  it('Позитивная проверка перехода на страницу "Group" без авторизации', () => {
+    cy.visit('https://ok.ru');
+    cy.xpath("//a[contains(text(),'Группы')]").should('be.visible').click({force:true});
+    cy.url().should('eq', 'https://ok.ru/groups');
+    cy.xpath("//span[text()='Создать группу']").should('be.visible');
+    cy.xpath(".//*[contains(@class, 'input__prt1l')]").should('be.visible');
+  });
+
+  it('Позитивная проверка перехода на страницу "Group" с авторизацией', () => {
+    cy.visit('https://ok.ru');
+    cy.get('#field_email').should('be.visible').as('emailInput');
+    cy.get('@emailInput').click({force:true});
+    cy.get('@emailInput').type(username);
+
+    cy.get('#field_password').should('be.visible').as('passwordInput');
+    cy.get('@passwordInput').click({force:true});
+    cy.get('@passwordInput').type(username);
+    cy.get('input.button-pro').scrollIntoView().click({ force: true });
+
+    cy.xpath("//a[contains(text(),'Группы')]").should('be.visible').click();
+    cy.url().should('eq', 'https://ok.ru/groups');
+    cy.xpath("//span[text()='Создать группу']").should('be.visible');
+    cy.xpath(".//*[contains(@class, 'input__prt1l')]").should('be.visible');
+    
+  });
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,5 @@
-module.exports = {
-    supportFile: 'cypress/support/e2e.js',
-};
+import 'cypress-xpath';
+
+// module.exports = {
+//     supportFile: 'cypress/support/e2e.js',
+// };
