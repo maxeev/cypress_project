d3ce70cbcb0ab5e53bc36051ec9b50b4316489c8
maxeev <kisel0478@mail.ru>
2025-05-29 12:09:02 +0300

    messagetest completed

--- cypress/e2e/ok/SendMessageTest.cy.js
+++ cypress/e2e/ok/SendMessageTest.cy.js
@@ -1,9 +1,10 @@
 describe('Send Message Test', () => {
     const loginURL = 'https://ok.ru/';
     const FRIEND_NAME = 'Максим Киселёв';
+    const MESSAGE = 'hello!'
 
     beforeEach(() => {
-        cy.log('jdjd');
+        cy.log('Открываем сайт и входим в акк перед каждым тестом');
         cy.visit(loginURL);
         cy.viewport(1920, 1080); //разрешение экрана для моего устройства
 
@@ -13,16 +14,15 @@ describe('Send Message Test', () => {
     });
 
     it('Проверка отправки сообщения', () => {
-        const message = 'привет';
-        const datePattern = 'HH:mm';
+  
+        cy.get('[data-l="t,messages"]').should('be.visible').click();
 
-        cy.log(`Начало теста отправки сообщения с текстом: '${message}'`);
-        cy.log(`Получатель: ${FRIEND_NAME}`);
+        cy.get('[data-tsid="chat-search-input"]').type(FRIEND_NAME, {force: true}); 
 
-        cy.get('[data-l="t,messages"').should('be.visible').click();
+        cy.get('[data-tsid="chat"]').first().click();
 
-        cy.get('[data-tsid="conversation_list"]', { timeout: 10000 }).should('be.visible'); // Ждать до 10 секунд
-        cy.get('[data-tsid="chat-search-input"]').should('be.visible').type(FRIEND_NAME);
-        cy.get('[data-tsid="chat"]').should('be.visible').click();
-    });
+        cy.get('[data-tsid="write_msg_input-input"]').type(MESSAGE);
+
+        cy.get('[data-l="t,sendButton"]').should('be.visible').first().click();
+});
 });
